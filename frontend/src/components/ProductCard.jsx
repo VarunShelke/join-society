d6d87d7770d899b7f0fbadcfc7c7485d4e1b3aa7
Varun Shelke <Varun.Shelke@hotmail.com>
2025-04-24 22:02:39 -0400

    Fixes for product image not loading.

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -9,7 +9,6 @@ const ProductCard = ({product}) => {
     const handleAddToCart = () => {
         if (!user) {
             toast.error("Please login to add products to cart", {id: "login"});
-            return;
         } else {
             // add to cart
             addToCart(product);
@@ -19,8 +18,9 @@ const ProductCard = ({product}) => {
     return (
         <div className='flex w-full relative flex-col overflow-hidden rounded-lg border border-gray-700 shadow-lg'>
             <div className='relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl'>
-                <img className='object-cover w-full' src={product.image} alt='product image'/>
-                <div className='absolute inset-0 bg-black bg-opacity-20'/>
+                <img className='w-full h-48 object-cover transition-transform duration-300 ease-in-out hover:scale-110'
+                     src={product.image} alt={product.name}/>
+
             </div>
 
             <div className='mt-4 px-5 pb-5'>
